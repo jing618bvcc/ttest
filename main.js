97eea074eaa9f3a4743f99bbb19dfdccbeeca542
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-06 13:32:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-/*
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-*/
 function preload(){
-    img=loadImage("YYuntech_map_E.jpg");
-    var o = document.getElementById("mm");
-    user1 = window.localStorage.getItem('manager');
-    if(user1 == 1){
-        o.style.display = '';
+  img=loadImage("YYuntech_map_E.jpg");
+ 
+  user1=window.localStorage.getItem('manager')
+ if(user1==1){
+     document.getElementById("mm").style.display='';
     }else{
-        o.style.display = 'none';
-    }
-    console.log(window.localStorage.getItem('manager'));
+     document.getElementById("mm").style.display='none';
+  }
+  console.log(window.localStorage.getItem('manager'));
+}
+function setup() {
+ createCanvas(1840/4, 2039/4);
+  
 }
 
-function setup(){
-    creatCanvas(1840/4,2039/4);
-}
-
-function draw(){
-    image(img,0,0,1840/4,2039/4);
+function draw() {
+  image(img,0,0,1840/4,2039/4);
+  
 }
