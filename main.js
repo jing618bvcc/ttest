a481bf51cd84a5e15c67384f61cd58334d0e862f
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-06 13:40:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -16,5 +16,11 @@ function setup() {
 
 function draw() {
   image(img,0,0,1840/4,2039/4);
-  
+  user1=window.localStorage.getItem('manager');
+ if(user1==1){
+     document.getElementById("mm").style.display='';
+    }else{
+     document.getElementById("mm").style.display='none';
+  }
+  console.log(window.localStorage.getItem('manager'));
 }
