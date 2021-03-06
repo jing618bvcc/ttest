90fd5bd1c7106a6da59b934289d700d479228a90
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-06 23:37:32 +0800

    Update book.js

--- book.js
+++ book.js
@@ -1,7 +1,20 @@
+function preload(){
+  var j=document.getElementById("join1");
+  user11=window.localStorage.getItem('username');
+  console.log(window.localStorage.getItem('username'))
+  if(user11==null){
+    j.style.display='none';
+  }
+  else{
+    j.style.display='';
+  }
+}
+
+
+
 function setup() {
-  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  
 }
