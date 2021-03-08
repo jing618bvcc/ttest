64195204fa361d18dead8e5106d1129da83b2e50
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-08 16:20:39 +0800

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,46 +1,49 @@
 let data2;
-let time=[];
-let title=[];
-let note=[];
-let hu=[];
+let data1;
+let data3;
+let jo=[];
+let ini=[];
+let ev=[];
+let date=[];
 let i=0;
-let b=[];
-let bas='<table>'+'<tr>'+'<th>'+"時間"+'</th>'+'<th>'+"標題"+'</th>'+'<th>'+"備註"+'</th>'+'<th>'+"已報名人數"+'</th>'+'</tr>';
+let event1=[];
 function preload(){
+  data3=loadJSON('https://spreadsheets.google.com/feeds/list/1_MXWO5Iz-fHN6Gp7uby4fpIbOgIK_bMpT9ZvfkktsHs/od6/public/values?alt=json')
   data2=loadJSON('https://spreadsheets.google.com/feeds/list/1ahCsxN9NJ5kCurS6WRCXBNzhjq_29_1yMMTOr0BnZrQ/od6/public/values?alt=json')
-  data1=loadJSON('https://spreadsheets.google.com/feeds/list/18t2EPSZbO8YNYMfURJBci1M8qSj2X_DmkgE8m49Y6sw/od6/public/values?alt=json')
+  
 }
 function setup() {
-  data2.feed.entry.forEach((c)=>{
-    time.push([c.gsx$時間戳記.$t])  
-    title.push([c.gsx$subject.$t]) 
-    note.push([c.gsx$description.$t])
-    note.push([c.gsx$description.$t])
-    hu.push([c.gsx$負責人人數.$t])
-    })
-  len=time.length
-  let fti=time.flat(Infinity);
-  let ftt=title.flat(Infinity);
-  let fno=note.flat(Infinity);
-  let fhu=hu.flat(Infinity);
-  document.write('<h3><a href = "index.html">回首頁</a></h3>')
+   data3.feed.entry.forEach((b)=>{
+    jo.push([ b.gsx$你想參加何時的共食呢.$t]);
+   })
+  data2.feed.entry.forEach((b)=>{
+    ev.push([ b.gsx$subject.$t]);
+    date.push([ b.gsx$startdate.$t]);
+   })
+  let fj=jo.flat(Infinity);
+  let fev=ev.flat(Infinity);
+  let fdate=date.flat(Infinity);
+  console.log(fj);
+  a=fj.length;
+  b=fev.length;
+  console.log(fdate);
+  for(i=0;i<b;i++){
+   ini.push(date[i])
+  }  
+  for(j=0;j<b;j++){
+    for(k=0;k<a;k++){
+      if(fj[k].includes(fdate[j])==true){
+       console.log(fj[k].includes(fdate[j]));
+        ini[j].push(fdate[j]);
+    }}
   
-  while( i<len ) {
-    b[i] = document.createElement("input");
-    b[i].type = 'button';
-    b[i].id = 'btn' + i;
-    b[i].value = "報名負責人"; 
-   document.write(bas+'<tr>'+'<td>' + fti[i] + '</td>'+'<td>' + ftt[i] + '</td>'+'<td>' + fno[i] + '</td>'+'<td>' + fhu[i] + '</td>'+'</tr>'+'</table>'); 
-    document.body.appendChild(b[i]); 
-    b[i].onclick = function () {
-     
-    }
-　i++;
-   }
- 
- 
- console.log(b[0])
-}                
-function draw() {
-
+  for(l=0;l<b;l++){
+    c=ini[l].length-1
+    event1.push("<h3>活動:"+fev[l]+"目前參加人數:"+c+"人"+"</h3><br>");
+  }
+   document.getElementById("event").innerHTML = event1;
 }
+  
+} 
+
+
