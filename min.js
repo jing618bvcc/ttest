f6cf60581b3edd4c744c5af95f64c643a63f90b9
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-12 23:53:44 +0800

    Update min.js

--- min.js
+++ min.js
@@ -8,15 +8,15 @@ function preload(){
      document.getElementById("mm").style.display='none';
   }
   if(user2==null){
-         document.getElementById("login").style.display='show';
-         document.getElementById("login1").style.display='show';
+         document.getElementById("login").style.display='';
+         document.getElementById("login1").style.display='';
          document.getElementById("mm1").style.display='none';
          document.getElementById("mm2").style.display='none';
    }else{
      document.getElementById("login").style.display='none'; 
      document.getElementById("login1").style.display='none';
-     document.getElementById("mm1").style.display='show';
-     document.getElementById("mm2").style.display='show';
+     document.getElementById("mm1").style.display='';
+     document.getElementById("mm2").style.display='';
   }    
 }
 function setup() {  
