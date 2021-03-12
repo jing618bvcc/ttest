2af455d09283ce814593196039caa63dbffda466
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-12 21:17:55 +0800

    Update min.js

--- min.js
+++ min.js
@@ -4,8 +4,10 @@ function preload(){
   user1=window.localStorage.getItem('manager');
  if(user1==1){
      document.getElementById("mm").style.display='';
+     document.getElementById("login").style.display='none';
     }else{
      document.getElementById("mm").style.display='none';
+     document.getElementById("login").style.display='show'; 
   }
   console.log(window.localStorage.getItem('manager'));
 }
