ce2f79435209e286aaa19131af32ec5197d9c40f
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-12 21:20:18 +0800

    Update min.js

--- min.js
+++ min.js
@@ -1,15 +1,17 @@
 function preload(){
   img=loadImage("YYuntech_map_E.jpg");
- 
+  user2=window.localStorage.getItem('username');
   user1=window.localStorage.getItem('manager');
  if(user1==1){
      document.getElementById("mm").style.display='';
-     document.getElementById("login").style.display='none';
-    }else{
+   }else{
      document.getElementById("mm").style.display='none';
-     document.getElementById("login").style.display='show'; 
   }
-  console.log(window.localStorage.getItem('manager'));
+  if(user2==1){
+         document.getElementById("login").style.display='none';
+   }else{
+     document.getElementById("login").style.display='show'; 
+  }    
 }
 function setup() {  
 }
