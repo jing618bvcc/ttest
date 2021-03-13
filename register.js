3d9625d8130d3b145b4853b31d1ad7fdbc9bc53e
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-13 17:56:16 +0800

    Update register.js

--- register.js
+++ register.js
@@ -16,25 +16,3 @@ function setup(){
 
 }
 
-/*
-let exeurl = 'https://script.google.com/macros/s/AKfycbzjxE18xRZiZyzSmNwVAo8RVVN4Ecjn3XAFTxnHhlD_o6geRWPkj9K55But98f8ny04mA/exec';
-let postData = {method:"write",id:"id", name:"name", mail_address:"mail_address"};
-
-function setup() {
-  //createCanvas(400, 400);
-  postBtn = createButton("Post Request");
-  postBtn.position(30, 60);
-  postBtn.mouseClicked(postRequest);
-}
-
-function postRequest() {
-  $.post(exeurl, postData,(data)=>{
-    console.log(data)
-  });
-}
-
-
-function draw() {
-  background(220);
-}
-*/
