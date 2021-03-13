b40108d717105ae4899c0e9bfc336b5c5a327d0b
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-13 18:27:52 +0800

    Update register.js

--- register.js
+++ register.js
@@ -3,14 +3,13 @@ function run(){
   password=document.getElementById("password").value.length;
   if(id>5){
         if(password>5){    alert("註冊成功");
-                   
                            window.location.href='https://jing618bvcc.github.io/ttest/';
                             
         }else{             alert("密碼至少6個字元");
-                           event.preventDefault();
+                           
              }
   }else{alert("帳號至少6個字元");
-         event.preventDefault();}
+         
 }
 function setup(){
 
