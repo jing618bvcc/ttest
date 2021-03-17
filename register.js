706b4cbfa3a62b7d869002f9b2a40e90d1498c2e
jing618bvcc <75161665+jing618bvcc@users.noreply.github.com>
2021-03-17 13:44:28 +0800

    Update register.js

--- register.js
+++ register.js
@@ -1,16 +1,19 @@
+let exeurl = 'https://script.google.com/macros/s/AKfycbzjxE18xRZiZyzSmNwVAo8RVVN4Ecjn3XAFTxnHhlD_o6geRWPkj9K55But98f8ny04mA/exec';
+let getID = document.getElementById("id");
+let postData = {method:"write", };
+let check;
+
 function run(){
-  id=document.getElementById("id").value.length;
   password=document.getElementById("password").value.length;
-  if(id>5){
-        if(password>5){    alert("註冊成功");
-                           window.location.href='https://jing618bvcc.github.io/ttest/';
-                          
-        }else{             alert("密碼至少6個字元");
-                           return false;
-             }
-  }else{alert("帳號至少6個字元");
-         return false;}
+  
+  if(password>=6){
+    $.post(exeurl, postData,(data)=>{
+      alert(data),check=1
+    })
+  }
+  setTimeout("javascript:location.href='index.html'", 3000);
 }
+
 function setup(){
 
 }
