4f6f22fa3119c831f1dc379fc1ebaaca5892c185
WeiChieh-Wang <75101293+WeiChieh-Wang@users.noreply.github.com>
2021-03-21 17:15:20 +0800

    Update min.js

--- min.js
+++ min.js
@@ -2,8 +2,8 @@ function preload(){
   img=loadImage("images/YYuntech_map_E.jpg");
   user2=window.localStorage.getItem('username');
   user1=window.localStorage.getItem('manager');
-  data3=loadJSON('https://spreadsheets.google.com/feeds/list/1_MXWO5Iz-fHN6Gp7uby4fpIbOgIK_bMpT9ZvfkktsHs/od6/public/values?alt=json')
-  data2=loadJSON('https://spreadsheets.google.com/feeds/list/1ahCsxN9NJ5kCurS6WRCXBNzhjq_29_1yMMTOr0BnZrQ/od6/public/values?alt=json')
+  data3=loadJSON('https://spreadsheets.google.com/feeds/list/1_MXWO5Iz-fHN6Gp7uby4fpIbOgIK_bMpT9ZvfkktsHs/od6/public/values?alt=json');
+  data2=loadJSON('https://spreadsheets.google.com/feeds/list/1ahCsxN9NJ5kCurS6WRCXBNzhjq_29_1yMMTOr0BnZrQ/od6/public/values?alt=json');
   GoogleJSON = loadJSON("https://spreadsheets.google.com/feeds/list/1LcYeY_pAKX4kyoUXDZjDA0-DFR5nDcpkuvbhy72JfnU/od6/public/values?alt=json");
   if(user1==1){
      document.getElementById("mm").style.display='';
